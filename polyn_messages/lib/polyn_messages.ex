39e4f9e11c3bded1e005e250613592195f6cfd31
brandynbennett <brandynlbennett@gmail.com>
2022-10-31 15:17:32 -0600

    polyn_messages boilerplate

---
+++ polyn_messages/lib/polyn_messages.ex
@@ -0,0 +1,18 @@
+defmodule PolynMessages do
+  @moduledoc """
+  Documentation for `PolynMessages`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PolynMessages.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
