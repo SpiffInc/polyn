ac017631a40121240d009b9dbbd40830883f4fa5
brandynbennett <brandynlbennett@gmail.com>
2022-10-31 16:42:38 -0600

    polyn_messages gets credo

--- polyn_messages/mix.exs
+++ polyn_messages/mix.exs
@@ -7,7 +7,8 @@ defmodule PolynMessages.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -23,7 +24,20 @@ defmodule PolynMessages.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.2"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      lint: ["credo --strict"]
     ]
   end
 end
