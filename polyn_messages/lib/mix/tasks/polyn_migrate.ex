1d6dbce33f6b7be14914a0df42270e8389c2e93e
brandynbennett <brandynlbennett@gmail.com>
2022-11-01 14:39:25 -0600

    start polyn migrate mix task

---
+++ polyn_messages/lib/mix/tasks/polyn_migrate.ex
@@ -0,0 +1,25 @@
+defmodule Mix.Tasks.Polyn.Migrate do
+  @moduledoc """
+  Updates JetStream which stream and consumer changes. Also updates the Polyn schema registry
+
+  ```bash
+  mix polyn.migrate
+  ```
+  """
+  @shortdoc "Migrate changes to schemas and NATS server"
+
+  use Mix.Task
+  require Mix.Generator
+
+  defstruct [:base_dir]
+
+  @impl Mix.Task
+  def run(args) do
+    {opts, _args} = OptionParser.parse!(args, strict: [dir: :string])
+
+    args =
+      struct!(__MODULE__,
+        base_dir: Keyword.get(opts, :dir, File.cwd!())
+      )
+  end
+end
