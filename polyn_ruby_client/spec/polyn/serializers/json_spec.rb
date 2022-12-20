c6c7a46cf8c0313a3f11a78e366b4f8cc79ad653
brandynbennett <brandynlbennett@gmail.com>
2022-12-20 13:57:01 -0700

    polyn ruby client expects only payload schema in store

--- polyn_ruby_client/spec/polyn/serializers/json_spec.rb
+++ polyn_ruby_client/spec/polyn/serializers/json_spec.rb
@@ -9,15 +9,10 @@ RSpec.describe Polyn::Serializers::Json do
   let(:schema_store) do
     Polyn::SchemaStore.new(nats, name: store_name, schemas: {
       "calc.mult.v1" => JSON.generate({
-        "type" => "object",
-        "properties": {
-          "data" => {
-            "type"       => "object",
-            "properties" => {
-              "a" => { "type" => "integer" },
-              "b" => { "type" => "integer" },
-            },
-          },
+        "type"       => "object",
+        "properties" => {
+          "a" => { "type" => "integer" },
+          "b" => { "type" => "integer" },
         },
       }),
     })
