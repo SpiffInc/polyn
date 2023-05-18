8f665b44d8620e41dc7f8f5f39a02f74569d4c15
brandynbennett <brandynlbennett@gmail.com>
2023-05-18 15:16:07 -0600

    add migration generator

---
+++ polyn_elixir_client/lib/mix/tasks/polyn.gen.migration.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Polyn.Gen.Migration do
+  @moduledoc """
+  Use `mix polyn.gen.migration <name>` to generate a new migration module for your application
+
+  The `<name>` argument should be the snake_cased name representing the change the migration will make.
+
+  The generated migration filename will be prefixed with the current timestamp in UTC which is used for versioning and ordering.
+  """
+  @shortdoc "Generates a new polyn migration file"
+
+  use Mix.Task
+
+  def run(args) do
+    parse_args(args)
+    |> Polyn.Migration.Generator.run()
+  end
+
+  defp parse_args(args) do
+    {options, [name]} = OptionParser.parse!(args, strict: [dir: :string])
+
+    %{
+      name: name,
+      dir: Keyword.get(options, :dir, Polyn.Migration.Migrator.migrations_dir())
+    }
+  end
+end
