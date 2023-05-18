ecc68d2712ac7c3ce8c5cba84576923ab37df238
brandynbennett <brandynlbennett@gmail.com>
2023-05-18 15:59:41 -0600

    add nats to dev config

--- polyn_elixir_client/config/dev.exs
+++ polyn_elixir_client/config/dev.exs
@@ -2,3 +2,10 @@ import Config
 
 config :polyn, :domain, "com.acme"
 config :polyn, :source_root, "user.backend"
+
+config :polyn, :nats, %{
+  name: :gnat,
+  connection_settings: [
+    %{host: "localhost", port: 4222}
+  ]
+}
