a237d1bfdd8d2ac603e9b968de282abd075afa11
brandynbennett <brandynlbennett@gmail.com>
2023-05-18 15:58:20 -0600

    make a function to wait for nats connection

--- polyn_elixir_client/lib/polyn/connection.ex
+++ polyn_elixir_client/lib/polyn/connection.ex
@@ -3,6 +3,31 @@ defmodule Polyn.Connection do
   @moduledoc false
 
   @doc """
+  The Gnat.Connection supervisor doesn't block on its startup. So
+  even if the application has started that doesn't mean the connection
+  is established. This function will block for a connection or timeout
+  """
+  def wait_for_connection do
+    task =
+      Task.async(fn ->
+        check_connection()
+      end)
+
+    Task.await(task)
+  end
+
+  defp wait_for_connection(result) do
+    case result do
+      nil -> check_connection()
+      _pid -> :ok
+    end
+  end
+
+  defp check_connection do
+    Process.whereis(name()) |> wait_for_connection()
+  end
+
+  @doc """
   Get the name of the NATS server connection
   """
   def name do
