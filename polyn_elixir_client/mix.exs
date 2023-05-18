2c780c69213ad168cf300bce28024819010f5c3f
brandynbennett <brandynlbennett@gmail.com>
2023-05-18 16:26:47 -0600

    bump version

--- polyn_elixir_client/mix.exs
+++ polyn_elixir_client/mix.exs
@@ -3,7 +3,7 @@ defmodule Polyn.MixProject do
 
   @github "https://github.com/SpiffInc/polyn/tree/main/polyn_elixir_client"
 
-  def version, do: "0.5.0"
+  def version, do: "0.6.0"
 
   def project do
     [
