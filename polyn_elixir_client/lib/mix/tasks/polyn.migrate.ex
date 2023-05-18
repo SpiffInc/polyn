c7847cf249be10c0afc9faeeb454c53d9e1d1434
brandynbennett <brandynlbennett@gmail.com>
2023-05-18 16:00:13 -0600

    add polyn.migrate mix task

---
+++ polyn_elixir_client/lib/mix/tasks/polyn.migrate.ex
@@ -0,0 +1,26 @@
+defmodule Mix.Tasks.Polyn.Migrate do
+  @moduledoc """
+  Use `mix polyn.migrate` to make configuration changes to your NATS server.
+  """
+  @shortdoc "Runs migrations to make modifications to your NATS Server"
+
+  use Mix.Task
+
+  def run(args) do
+    {:ok, _apps} = Application.ensure_all_started(:polyn)
+
+    :ok = Polyn.Connection.wait_for_connection()
+
+    parse_args(args)
+    |> Polyn.Migration.Migrator.run()
+  end
+
+  defp parse_args(args) do
+    {options, []} = OptionParser.parse!(args, strict: [migrations_dir: :string])
+
+    [
+      migrations_dir:
+        Keyword.get(options, :migrations_dir, Polyn.Migration.Migrator.migrations_dir())
+    ]
+  end
+end
